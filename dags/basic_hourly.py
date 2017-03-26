9bc0f989036d6d5789af50d008c56fb61ad28a58
isaacmg <igodfried@isaac26.com>
2017-03-26 04:52:30 -0400

    dags and cleanup

---
+++ dags/basic_hourly.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators import BashOperator
+from datetime import datetime
+import os
+import sys
+
+args = {
+  'owner': 'airflow'
+  , 'start_date': datetime(2017, 1, 27)
+  , 'provide_context': True
+}
+d = datetime(2017, 1, 17, 3, 15,00)
+
+
+dag = DAG('run_facebook', start_date = d, schedule_interval = '16 * * * *', default_args = args)
+
+t_main = BashOperator(
+  task_id = 'usgs_fetch'
+  , dag = dag
+  , bash_command = 'python Documents/Tech/scraper/get_posts.py'
+  )
+
--- tests/fb_posts_test.py
+++ tests/fb_posts_test.py
@@ -25,7 +25,7 @@ class MyTest(unittest.TestCase):
         d["13142345"] = timestamp
         self.assertEqual(get_tstamp("13142345", 1,"test"), str(timestamp))
     def test_access_t(self):
-        self.assertEquals(get_access("54449",'text.txt'),"54449|awerqwerdummytext")
+        self.assertEquals(get_access("54449",'files/text.txt'),"54449|awerqwerdummytext")
     def test_getFacebookPageFeedData(self):
         access = "238791666290359|" + os.environ['FB_KEY']
         data = getFacebookPageFeedData("176485839144245", access, 100, 0 )
