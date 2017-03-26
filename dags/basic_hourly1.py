92592e3200c1443ded08cf2f32e3fd794e6fc760
isaacmg <igodfried@isaac26.com>
2017-03-26 19:52:04 -0400

    cleanup

--- dags/basic_hourly1.py
+++ dags/basic_hourly1.py
@@ -9,7 +9,7 @@ args = {
   , 'start_date': datetime(2017, 1, 27)
   , 'provide_context': True
 }
-d = datetime(2017, 1, 17, 3, 15,00)
+d = datetime(2017, 3, 25, 3, 15,00)
 
 
 dag = DAG('run_facebook', start_date = d, schedule_interval = '16 * * * *', default_args = args)
@@ -17,6 +17,5 @@ dag = DAG('run_facebook', start_date = d, schedule_interval = '16 * * * *', defa
 t_main = BashOperator(
     task_id = 'fb_scrape'
   , dag = dag
-  , bash_command = 'python Documents/Tech/scraper/get_posts.py'
+  , bash_command = 'python /Users/isaac/Documents/Tech/fb_scraper/get_posts.py'
   )
-
