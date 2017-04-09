fc68374ff40eb46fb1a8e8ce38bc7ace576f5d15
isaacmg <igodfried@isaac26.com>
2017-04-09 01:36:52 -0400

    Added dag
    Added dag for running with variables/d

---
+++ airflow/dags/fb_docker.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+import os
+import sys
+
+args = {
+  'owner': 'airflow'
+  , 'start_date': datetime(2017, 1, 27)
+  , 'provide_context': True
+}
+d = datetime(2017, 3, 25, 3, 15,00)
+
+
+dag = DAG('run_facebook', start_date = d, schedule_interval = '10 * * * *', default_args = args)
+
+t_main = BashOperator(
+    task_id = 'fb_scrape'
+  , dag = dag
+  , bash_command = 'docker run --env-file variables.list paddlesoft/fb-scraping'
+  )
