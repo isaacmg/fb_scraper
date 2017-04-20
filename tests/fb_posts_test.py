682bec9dd844074d5417e5b437f4722a830addd2
isaacmg <igodfried@isaac26.com>
2017-04-20 04:21:06 -0400

    fuckin unit

--- tests/fb_posts_test.py
+++ tests/fb_posts_test.py
@@ -77,7 +77,7 @@ class MyTest(unittest.TestCase):
     def test_groups_pages(self):
         group_id = "paddlesoft"
         self.assertEqual(scrape_groups_pages(group_id, 0,  False), "Sucessfully scraped from 0for page id paddlesoft")
-        self.assertEqual(scrape_groups_pages(group_id, 0, True), "Sucessfully scraped from 0page id paddlesoft")
+        self.assertEqual(scrape_groups_pages(group_id, 1, False), "Sucessfully scraped from 1for page id paddlesoft")
 
     def test_serialize(self):
         testList = ('115285708497149_1731636350195402', 'One-day special session, Northampton, MA YMCA.','Northampton Pool Rolling Session', 'event', 'https://www.facebook.com/events/319137738480268/','2017-01-09 18:51:17', 1, 0, 0, 1, 0, 0, 0, 0, 0)
