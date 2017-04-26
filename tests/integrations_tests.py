fa02eb8e0bd043293eb96a1be93ee88bea6084b2
isaacmg <igodfried@isaac26.com>
2017-04-25 22:17:16 -0400

    Clean up messy syntax

--- tests/integrations_tests.py
+++ tests/integrations_tests.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-from fb_posts import scrape_comments_from_last_scrape, scrape_posts_from_last_scrape, scrape_posts_from_last_scrape_kafka, scrape_all_comments
+from fb_posts import *
 scrape_posts_from_last_scrape("115285708497149")
 scrape_posts_from_last_scrape_kafka("319872211700098")
 scrape_all_comments("319872211700098")
