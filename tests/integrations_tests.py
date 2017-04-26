b59126299de8e520ce31096ab70ab5610bcc6d34
isaacmg <igodfried@isaac26.com>
2017-04-25 21:29:57 -0400

    Fix ids in integration

--- tests/integrations_tests.py
+++ tests/integrations_tests.py
@@ -1,4 +1,4 @@
 from fb_posts import scrape_comments_from_last_scrape, scrape_posts_from_last_scrape, scrape_posts_from_last_scrape_kafka, scrape_all_comments
 scrape_posts_from_last_scrape("115285708497149")
-scrape_posts_from_last_scrape_kafka("oronopaddlers")
-scrape_all_comments("oronopaddlers")
+scrape_posts_from_last_scrape_kafka("319872211700098")
+scrape_all_comments("319872211700098")
