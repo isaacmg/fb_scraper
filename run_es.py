f923e8264512dd33db54a1085c840f409bcefcc9
isaacmg <igodfried@isaac26.com>
2017-04-28 02:30:57 -0400

    pushing again

--- run_es.py
+++ run_es.py
@@ -3,7 +3,7 @@ from requests_aws4auth import AWS4Auth
 import os
 def init_es():
     host = os.environ['ES_HOST']
-    if os.environ['ES_USE_AWS'] is 1:
+    if os.environ['ES_USE_AWS'] is "1":
         awsauth = AWS4Auth(os.environ['AWS_ES_ID'], os.environ['AWS_SECRET_ES'], ['AWS_ES_REGION'], 'es')
     es = Elasticsearch(
         hosts=[{'host': host, 'port': 443}],
