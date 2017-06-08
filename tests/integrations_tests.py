b2ccce493fa336ea68fd5be7e00980b374073272
isaacmg <igodfried@isaac26.com>
2017-06-08 04:29:41 -0400

    all posts replace comm

--- tests/integrations_tests.py
+++ tests/integrations_tests.py
@@ -5,4 +5,5 @@ from get_posts import *
 scrape_posts_last_es("115285708497149")
 scrape_posts_from_last_scrape("115285708497149")
 scrape_posts_from_last_scrape_kafka("319872211700098")
-scrapeComments("115285708497149")
+scrape_all_posts("319872211700098")
+#TODO ADD TEST SPECIFICALLY FOR COMMENTS 
