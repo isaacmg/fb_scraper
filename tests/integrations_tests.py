eb0dcbd94811a2ada31fb86a60e9de27f8a5f57c
isaacmg <igodfried@isaac26.com>
2017-06-08 04:17:08 -0400

    Added scraping com test

--- tests/integrations_tests.py
+++ tests/integrations_tests.py
@@ -5,3 +5,4 @@ from get_posts import *
 scrape_posts_last_es("115285708497149")
 scrape_posts_from_last_scrape("115285708497149")
 scrape_posts_from_last_scrape_kafka("319872211700098")
+scrapeComments("115285708497149")
