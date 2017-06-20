55a8b5f651113c8f7ce2223a2c9b965e5ca6795c
isaacmg <igodfried@isaac26.com>
2017-06-20 16:16:54 -0400

    Fixed small error

--- pg_db.py
+++ pg_db.py
@@ -11,6 +11,6 @@ class Record(db.Entity):
 db.generate_mapping(create_tables=True)
 
 @db_session
-def save_scrape_PS(startTime, numberScraped, group):
+def save_scrape_PS(startTime, endTime, numberScraped, group):
     timestamp = int(time.time())
     Record(group_name = group, unix_tstamp=timestamp, end_tstamp=startTime, posts_scraped=numberScraped)
