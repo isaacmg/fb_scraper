65df66ee0fc89aa006f3241871b58cb6f6a67557
isaacmg <igodfried@isaac26.com>
2017-06-20 13:28:49 -0400

    fixed small error

--- pg_db.py
+++ pg_db.py
@@ -1,6 +1,6 @@
 from pony.orm import *
 import os
-db = pony.Database("postgres", user=os.environ['pg_user'], password=os.environ['pg_password'], host=os.environ['pg_host'], database=os.environ['pg_db'])
+db = Database("postgres", user=os.environ['pg_user'], password=os.environ['pg_password'], host=os.environ['pg_host'], database=os.environ['pg_db'])
 class Record(db.Entity):
     id = PrimaryKey(int, auto=True)
     group_name = Required(str)
